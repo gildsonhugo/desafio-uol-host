c2d4ebc2b905c531e330f6f69f8af3ae34df1d5b
gildsonhugo <gildson.hugo@gmail.com>
2019-09-24 21:02:15 -0300

    Adicionando comentarios

--- src/app/filme/filme.component.ts
+++ src/app/filme/filme.component.ts
@@ -12,10 +12,12 @@ import {Personagem} from '../models/personagem';
 })
 export class FilmeComponent implements OnInit, OnDestroy{
 
+  /*VARIAVEIS DE EXIBICAO, LISTAGEM, E BUSCA*/
   filmeId: number;
   filmeObject: Filme;
   listPersonagens: Array<Personagem>;
 
+  /*VARIAVEIS DE SUBSCRIPTIONS*/
   routeSubscription: Subscription;
   filmesSubscription: Subscription;
   personagensSubscription: Subscription;
@@ -31,22 +33,26 @@ export class FilmeComponent implements OnInit, OnDestroy{
     this.getPersonagensListFromService();
   }
 
+  /*METODO QUE BUSCA A LISTA DE FILMES DO SERVICO, E RETORNA O OBJETO QUE CONTEM DETERMINADO ID*/
   getFilmeFromService(id: number){
     this.filmesSubscription = this.service.getFilmesFromJsonArchive().subscribe((res: any) => {
       this.filmeObject = res.results.find(filme => filme.id == id);
     });
   }
 
+  /*BUSCA A LISTA DE PERSONAGENS DO SERVICO*/
   getPersonagensListFromService(){
     this.personagensSubscription = this.service.getPersonagensFromJsonArchive().subscribe((res: any) => {
       this.listPersonagens = res.results;
     });
   }
 
+  /*BUSCA O OBJETO DO PERSONAGEM PELO ID*/
   getPersonagemById(id: number){
      return this.listPersonagens.find(personagem => personagem.id == id);
   }
 
+  /*UNSUBSCRIBES*/
   ngOnDestroy(): void {
     this.routeSubscription.unsubscribe();
     this.filmesSubscription.unsubscribe();
--- src/app/personagens/personagens.component.ts
+++ src/app/personagens/personagens.component.ts
@@ -11,10 +11,12 @@ import {Subscription} from 'rxjs';
 })
 export class PersonagensComponent implements OnInit, OnDestroy {
 
+  /*VARIAVEIS DE LISTAGEM E BUSCA*/
   listPersonagens: Array<Personagem>;
   listFilmes: Array<Filme>;
   searchString = {name: ''};
 
+  /*VARIAVEIS DE SUBSCRIPTIONS*/
   personagensSubscription: Subscription;
   filmesSubscription: Subscription;
 
@@ -25,6 +27,7 @@ export class PersonagensComponent implements OnInit, OnDestroy {
     this.getFilmesListFromService();
   }
 
+  /*BUSCA A LISTA DE PERSONAGENS DO SERVICO E ORDENA PELA QUANTIDADE DE FILMES, E PELO NOME, CASO TENHA A MESMA QUANTIDADE DE FILMES*/
   getPersonagensListFromService(){
     this.personagensSubscription = this.service.getPersonagensFromJsonArchive().subscribe((res: any) => {
        this.listPersonagens = res.results.sort((a, b) => {
@@ -43,21 +46,24 @@ export class PersonagensComponent implements OnInit, OnDestroy {
     });
   }
 
+  /*BUSCA A LISTA DE FILMES CHAMANDO O METODO DO SERVICO*/
   getFilmesListFromService(){
     this.filmesSubscription = this.service.getFilmesFromJsonArchive().subscribe((res: any) => {
       this.listFilmes = res.results;
     });
   }
 
-  ngOnDestroy(): void {
-    this.personagensSubscription.unsubscribe();
-    this.filmesSubscription.unsubscribe();
-  }
-
+  /*BUSCA O OBJETO DO FILME PELO O ID*/
   filterFilmeById(id): Filme{
     if(this.listFilmes){
       return this.listFilmes.find(filme => filme.id == id);
     }
+  }
+
+  /*UNSUBSCRIBES*/
+  ngOnDestroy(): void {
+    this.personagensSubscription.unsubscribe();
+    this.filmesSubscription.unsubscribe();
   }
 
 }
